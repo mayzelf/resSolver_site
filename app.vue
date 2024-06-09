c474f8d138df9535d33e60aaecf0ecd210c958ba
justno <120106323+mayzelf@users.noreply.github.com>
2024-06-09 17:20:06 +0200

    added modules

--- app.vue
+++ app.vue
@@ -1,5 +1,21 @@
 <template>
   <div>
-    <NuxtWelcome />
+    
   </div>
 </template>
+
+<script>
+export default {
+  head: {
+    htmlAttrs: {
+      lang: 'en'
+    },
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ],
+    meta: [
+      { hid: 'language', name: 'language', content: 'en' }
+    ]
+  }
+}
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/color-mode", "@vesp/nuxt-fontawesome"],
+
+  colorMode: {
+    classSuffix: "",
+  },
+
+  tailwindcss: {
+    configPath: 'tailwind.config.js',
+  },
+
+  fontawesome: {
+    icons: {
+      solid: ["user", "envelope", "key", "tag", "dice", "arrow-up-from-bracket", "spinner", "trash-can", "fa-times"],
+      regular: ["user", "sun", "moon"],
+      brands: ["discord", "github", "steam", "youtube", "reddit", "twitch"],
+    },
+    content: {},
+  } as { [key: string]: unknown },
+
+});
