c3db248ba62b56c139c6c3a5dc5a6115fea5ce13
justno <120106323+mayzelf@users.noreply.github.com>
2024-07-16 17:13:29 +0200

    added legal stuff

---
+++ components/Footer.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="dark:bg-gray-800 dark:text-white bg-white text-black text-center p-4 relative">
+        <div class="container mx-auto">
+            <div class="flex justify-center space-x-4">
+                <a href="https://mayzelf.xyz/imprint" class="font-bold hover:text-blue-500 hover:underline inline-flex items-center px-1 pt-1 text-sm text-gray-900 dark:text-white">
+                    Imprint
+                </a>
+                <a href="https://mayzelf.xyz/privacypolicy" class="font-bold hover:text-blue-500 hover:underline inline-flex items-center px-1 pt-1 text-sm text-gray-900 dark:text-white">
+                    Privacy Policy
+                </a>
+            </div>
+        </div>
+    </div>
+</template>
